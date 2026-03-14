84151cc2ce76d9bceaf1e10afb7b94f5d1baa7d9
admltfr <adamlutfiramadhan@gmail.com>
2026-03-14 11:34:02 +0700

    chore:initialize dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
+import 'package:halositek/app/data/network/token_service.dart';
 import 'app/routes/app_pages.dart';
+import 'app/data/network/api_client.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
+  final ApiClient apiClient = ApiClient();
+  final TokenService tokenService = TokenService();
+
   runApp(
     GetMaterialApp(
       title: "Application",
       initialRoute: AppPages.INITIAL,
+      initialBinding: BindingsBuilder(() {
+        Get.put(tokenService, permanent: true);
+        Get.lazyPut<ApiClient>(() => apiClient, fenix: true);
+      }),
       getPages: AppPages.routes,
     ),
   );
